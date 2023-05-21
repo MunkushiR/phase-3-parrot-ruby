7945a841e4046143ef91724ccf1b980c58698161
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-05-21 14:51:30 +0300

    done

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+require 'pry'
+
+def parrot name=("Squawk!")
+    puts name
+    binding.pry
+    return name
+end
+
+parrot
